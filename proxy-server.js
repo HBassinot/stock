6109e8d95101006559f749a0510b9d19f664ce37
hbassinot <herve.bassinot@gmail.com>
2024-02-22 18:45:15 -0500

    add v2

--- proxy-server.js
+++ proxy-server.js
@@ -4,7 +4,10 @@ const https = require('https');
 const fs = require('fs');
 
 const app = express();
-const port = 8000;
+const port = 443;
+
+// Chemin vers le fichier JSON
+const stocksPath = './public/config.json';
 
 // Middleware pour permettre l'accès CORS (Cross-Origin Resource Sharing)
 app.use((req, res, next) => {
@@ -18,44 +21,145 @@ const httpsOptions = {
   cert: fs.readFileSync('server-cert.pem')
 };
 
+
 // Route pour appeler le web service tiers
+app.get('/portfolio/values', async (req, res) => {
+  let portfoliosValue = [];
+
+  // Lecture du fichier JSON
+  const objetJSON = require(stocksPath);
+  if (objetJSON) {
+    console.log("Contenu du fichier JSON:", objetJSON);
+  } else {
+    console.log("Les données JSON ne sont pas encore disponibles.");
+  }
+
+  for (const portfolio of objetJSON.portfolios) {
+    let portfolioValue = {};
+    var totalPortfolioPrice = 0;
+
+    for (const action of portfolio.actions) {
+      let cur = 1;
+      const stockPrice = await getStockPrice(action.nom);
+      if(stockPrice.currency != "CAD") {
+        const currencyPrice = await getCurrencyPrice(stockPrice.currency+"CAD");
+        cur = currencyPrice.price;
+      }
+
+      totalPortfolioPrice += stockPrice.price * cur * action.quantite;
+    }
+    portfolioValue = {[portfolio.name] : totalPortfolioPrice};
+    portfoliosValue.push(portfolioValue);
+  }
+
+  res.json(portfoliosValue);
+});
+
+
+
+// Route pour appeler le web service tiers
+app.get('/portfolio/total', async (req, res) => {
+  var totalStockPrice = 0;
+
+  // Lecture du fichier JSON
+  const objetJSON = require(stocksPath);
+  if (objetJSON) {
+    console.log("Contenu du fichier JSON:", objetJSON);
+  } else {
+    console.log("Les données JSON ne sont pas encore disponibles.");
+  }
+
+  for (const portfolio of objetJSON.portfolios) {
+    for (const action of portfolio.actions) {
+      let cur = 1;
+      const stockPrice = await getStockPrice(action.nom);
+      if(stockPrice.currency != "CAD") {
+        const currencyPrice = await getCurrencyPrice(stockPrice.currency+"CAD");
+        cur = currencyPrice.price;
+      }
+
+      totalStockPrice += stockPrice.price * cur * action.quantite;
+    }
+  }
+
+  res.json(totalStockPrice);
+});
+
+
+// Route ou service utilisant la fonction getStockPrice
 app.get('/stock/price/:stock', async (req, res) => {
   const stock = req.params.stock;
   try {
-    // Appel du web service tiers
-    stocksplit = stock.split(":")[0];
-    const response = await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${stocksplit}`);
-    
-    // Récupération du prix de l'action depuis la réponse
-    const price = response.data.chart.result[0].meta.regularMarketPrice;
-    const currency = response.data.chart.result[0].meta.currency;
-
-    // Envoi du prix de l'action en réponse
-    res.json({ price, currency });
+      // Appel de la fonction pour récupérer le prix de l'action
+      const stockPrice = await getStockPrice(stock);
+      
+      // Envoi du prix de l'action en réponse
+      res.json(stockPrice);
   } catch (error) {
-    console.error('Erreur lors de la récupération du prix de l\'action :', error);
-    res.status(500).json({ error: 'Erreur lors de la récupération du prix de l\'action' });
+      // Gestion des erreurs
+      console.error('Erreur lors de la récupération du prix de l\'action :', error);
+      res.status(500).json({ error: 'Erreur lors de la récupération du prix de l\'action' });
   }
 });
 
-// Route pour appeler le web service tiers
+
+
+// Route ou service utilisant la fonction getCurrencyPrice
 app.get('/currency/:currency', async (req, res) => {
   const currency = req.params.currency;
   try {
-    // Appel du web service tiers
-    currencysplit = currency.split(":")[0];
-    const response = await axios.get(`https://query1.finance.yahoo.com/v7/finance/spark?symbols=${currencysplit}%3DX`);
-    
-    // Récupération du prix de l'action depuis la réponse
-    const price = response.data.spark.result[0].response[0].meta.regularMarketPrice;
-
-    // Envoi du prix de l'action en réponse
-    res.json({ price });
+      // Appel de la fonction pour récupérer le prix de la devise
+      const currencyPrice = await getCurrencyPrice(currency);
+      
+      // Envoi du prix de la devise en réponse
+      res.json(currencyPrice);
   } catch (error) {
-    console.error('Erreur lors de la récupération du prix de l\'action :', error);
-    res.status(500).json({ error: 'Erreur lors de la récupération du prix de l\'action' });
+      // Gestion des erreurs
+      console.error('Erreur lors de la récupération du prix de la devise :', error);
+      res.status(500).json({ error: 'Erreur lors de la récupération du prix de la devise' });
   }
 });
+
+
+// Définition de la fonction pour récupérer le prix de l'action
+async function getStockPrice(stock) {
+  try {
+      // Appel du web service tiers
+      const response = await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${stock}`);
+      
+      // Récupération du prix de l'action depuis la réponse
+      const price = response.data.chart.result[0].meta.regularMarketPrice;
+      const currency = response.data.chart.result[0].meta.currency;
+
+      // Retourne le prix de l'action et la devise
+      return { price, currency };
+  } catch (error) {
+      // Gestion des erreurs
+      console.error('Erreur lors de la récupération du prix de l\'action :', error);
+      throw new Error('Erreur lors de la récupération du prix de l\'action');
+  }
+}
+
+
+// Définition de la fonction pour récupérer le prix de la devise
+async function getCurrencyPrice(currency) {
+  try {
+      // Appel du web service tiers
+      const currencySplit = currency.split(":")[0];
+      const response = await axios.get(`https://query1.finance.yahoo.com/v7/finance/spark?symbols=${currencySplit}%3DX`);
+      
+      // Récupération du prix de la devise depuis la réponse
+      const price = response.data.spark.result[0].response[0].meta.regularMarketPrice;
+
+      // Retourne le prix de la devise
+      return { price };
+  } catch (error) {
+      // Gestion des erreurs
+      console.error('Erreur lors de la récupération du prix de la devise :', error);
+      throw new Error('Erreur lors de la récupération du prix de la devise');
+  }
+}
+
 
 // Démarrage du serveur
 // Création du serveur HTTPS
--- src/App.js
+++ src/App.js
@@ -6,30 +6,22 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  let domain = "stock.hbassinot.com";
+  let port = 443;
+
+  if(false) {
+    domain = "localhost";
+  }
   const [portefeuille, setPortefeuille] = useState(0);
 
   const fetchData = async () => {
     try {
-      // Charger les données de configuration depuis le fichier config.json
-      const configResponse = await axios.get('/config.json');
-      const config = configResponse.data;
 
-      // Vérifier que config.actions est bien un tableau
-      if (!Array.isArray(config.actions)) {
-        throw new Error("Les données de configuration ne sont pas valides");
-      }
-     
-      // Calculer la valeur totale du portefeuille en se basant sur les données de la bourse
-      let valeurTotale = 0;
-      for (const action of config.actions) {
-        
-        //valeurTotale += getPrixStock(action.nom, prixAction) * action.quantite;
-        getPrixActionsYahooFinance(action.nom).then(price => {
-          valeurTotale += price * action.quantite;
-          setPortefeuille(valeurTotale);
-        })
-      }
       
+      // callStock() V1
+      callPortfolios(); // V2
+
+
     } catch (error) {
       console.error('Erreur lors de la récupération des données :', error);
     }
@@ -38,35 +30,94 @@ function App() {
   useEffect(() => {
     fetchData();
 
-    const interval = setInterval(fetchData, 10000); // Rafraîchir toutes les 100 secondes
+    const interval = setInterval(fetchData, 15000); // Rafraîchir toutes les 15 secondes
     return () => clearInterval(interval);
   }, []);
 
-  const getPrixStock = (stock, prixAction) => {
-    for (const action of prixAction) {
-      if(action.symbol === stock) {
-        return action.price;
+  /*** V2 */
+
+  const callPortfolios = async () => {
+    let valeurTotale = 0;
+
+    getPrixPortfoliosHBassinot().then(portfolios => {
+      for (let i = 0; i < portfolios.length; i++) {
+        const keys = Object.keys(portfolios[i]);
+        valeurTotale += portfolios[i][keys[0]];
+       
       }
-    }
-    return 0;
+
+      const montantElement = document.getElementById('montant');
+    
+      // Ajouter la classe d'effet de clignotement
+      montantElement.classList.add('blink');
+  
+      setPortefeuille(valeurTotale);
+      
+    
+      // Supprimer la classe d'effet de clignotement après 500ms
+      setTimeout(() => {
+        montantElement.classList.remove('blink');
+      }, 1500);
+
+
+    });
   }
 
-  const getPrixActionsYahooFinance = async (stock) => {
-    const response = await axios.get(`https://stock.hbassinot.com:8000/stock/price/${stock}`);
+  const getPrixPortfoliosHBassinot = async () => {
+    const response = await axios.get(`https://`+domain+`:`+port+`/portfolio/values`);
+    return response.data; 
+  };
+
+  /********/
+
+  /*** V1 */
+
+  const callStock = async () => {
+    // Charger les données de configuration depuis le fichier config.json
+    const configResponse = await axios.get('/config.json');
+    const config = configResponse.data;
+  
+    // Vérifier que config.actions est bien un tableau
+    if (!Array.isArray(config.portfolios)) {
+      throw new Error("Les données de configuration ne sont pas valides");
+    }
+    
+    // Calculer la valeur totale du portefeuille en se basant sur les données de la bourse
+    let valeurTotale = 0;
+    for (const portfolio of config.portfolios) {
+      for (const action of portfolio.actions) {
+        
+        //valeurTotale += getPrixStock(action.nom, prixAction) * action.quantite;
+        getPrixActionsHBassinot(action.nom).then(price => {
+          valeurTotale += price * action.quantite;
+          setPortefeuille(valeurTotale);
+        })
+      }
+    }
+  }
+
+  const getPrixActionsHBassinot = async (stock) => {
+    const response = await axios.get(`https://`+domain+`:`+port+`/stock/price/${stock}`);
     let cur = 1;
     if(response.data.currency === "USD") {
-      const usdcar = await axios.get(`https://stock.hbassinot.com:8000/currency/USDCAD`);
+      const usdcar = await axios.get(`https://`+domain+`:`+port+`/currency/USDCAD`);
       cur = usdcar.data.price;
     }
     return response.data.price * cur; 
   };
 
+  /*******/
+
   return (
     <div className="App">
       <h1>Portefeuille en temps réel</h1>
-      <div className='container'><div className='montant'>{portefeuille.toFixed(2)} $</div></div>
+      <div className='container'><div className='montant' id='montant'>{portefeuille.toFixed(2)} $</div></div>
     </div>
   );
 }
+
+
+
+
 
 export default App;
